15b96d5dd3d831c20e205f137a95d05cc4ad382d
Shiv Kumar Singh <shiv.kumar@mscorpres.in>
2024-12-14 21:04:13 +0530

    59

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -8,22 +8,22 @@ ipcRenderer.invoke('get-app-version').then((version) => {
 });
 
 // Fetch and update server status
-// ipcRenderer.invoke('get-server-status').then((status) => {
-//     document.getElementById('serverStatus').innerText = status ? 'Online' : 'Offline';
-// }).catch(() => {
-//     document.getElementById('serverStatus').innerText = 'Offline';
-// });
+ipcRenderer.invoke('get-server-status').then((status) => {
+    document.getElementById('serverStatus').innerText = status ? 'Online' : 'Offline';
+}).catch(() => {
+    document.getElementById('serverStatus').innerText = 'Offline';
+});
 
 
 // Fetch and update printer status
-// ipcRenderer.invoke('get-printer-info')
-//     .then((printer) => {
-//         document.getElementById('printerStatus').innerText = printer || 'NA';
-//     })
-//     .catch((error) => {
-//         console.error('Error fetching printer status:', error);
-//         document.getElementById('printerStatus').innerText = 'NA';
-//     });
+ipcRenderer.invoke('get-printer-info')
+    .then((printer) => {
+        document.getElementById('printerStatus').innerText = printer || 'NA';
+    })
+    .catch((error) => {
+        console.error('Error fetching printer status:', error);
+        document.getElementById('printerStatus').innerText = 'NA';
+    });
 
 
 // Minimize window functionality
@@ -37,15 +37,15 @@ document.getElementById('close-link').addEventListener('click', () => {
 });
 
 // Fetch and update network info (MAC address and IP address)
-// ipcRenderer.invoke('get-network-info').then((networkInfo) => {
-//     const { macAddress, ipAddress } = networkInfo;
+ipcRenderer.invoke('get-network-info').then((networkInfo) => {
+    const { macAddress, ipAddress } = networkInfo;
 
-//     document.getElementById('macAddress').innerText = macAddress || 'N/A';
-//     document.getElementById('ipAddress').innerText = ipAddress || 'N/A';
-// }).catch(() => {
-//     document.getElementById('macAddress').innerText = 'N/A';
-//     document.getElementById('ipAddress').innerText = 'N/A';
-// });
+    document.getElementById('macAddress').innerText = macAddress || 'N/A';
+    document.getElementById('ipAddress').innerText = ipAddress || 'N/A';
+}).catch(() => {
+    document.getElementById('macAddress').innerText = 'N/A';
+    document.getElementById('ipAddress').innerText = 'N/A';
+});
 
 
 function updateTaskbarProgress(percent) {
